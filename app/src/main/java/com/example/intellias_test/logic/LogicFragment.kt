2f3457050ae6e7ebb8f02d10df265c01b28cdcc0
unknown <giomozda74@gmail.com>
2021-12-02 13:51:48 +0200

    add data binding

--- app/src/main/java/com/example/intellias_test/logic/LogicFragment.kt
+++ app/src/main/java/com/example/intellias_test/logic/LogicFragment.kt
@@ -6,21 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.intellias_test.R
 import com.example.intellias_test.databinding.FragmentLogicBinding
+import kotlinx.android.synthetic.main.fragment_logic.view.*
 
 
 class LogicFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentLogicBinding
+    private val viewModel by viewModels<LogicViewModel>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentLogicBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_logic, container, false
         )
+
+        binding.logicViewModel = viewModel
+        binding.setLifecycleOwner(this)
+
+        binding.wordText.setOnClickListener {
+            viewModel.setWord(it.word_text.text.toString())
+        }
+
+        /*viewModel.word.observe(this, Observer { newWord ->
+            viewModel.wordInput(newWord)
+        })*/
         return binding.root
     }
 
--- app/src/main/java/com/example/intellias_test/logic/LogicViewModel.kt
+++ app/src/main/java/com/example/intellias_test/logic/LogicViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.intellias_test.logic
 
-class LogicViewModel {
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LogicViewModel: ViewModel() {
+
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
+
+    private val _description = MutableLiveData<String>()
+    val description : LiveData<String>
+        get() = _description
+
+    init {
+        _word.value = ""
+        _description.value = ""
+    }
+
+
+    //take word from view
+    fun setWord(newWord: String) {
+        _word.value = newWord
+    }
+
+    fun enterButtonClick() {
+
+        _description.value = _word.value
+    }
 }
